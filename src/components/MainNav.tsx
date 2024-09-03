8a28bf6039c0f8e6540bf172f9cbe20d0a2db961
Neeraj Singh <869neerajsingh@gmail.com>
2024-09-03 16:48:50 +0530

    feat: create and integrate username menu component in main nav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,17 +1,25 @@
 import { useAuth0 } from "@auth0/auth0-react";
+
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
-    <Button
-      variant="ghost"
-      className="hover:bg-white hover:text-blue-500 text-md font-bold"
-      onClick={() => loginWithRedirect()}
-    >
-      Log In
-    </Button>
+    <>
+      {isAuthenticated ? (
+        <UsernameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="hover:bg-white hover:text-blue-500 text-md font-bold"
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
+    </>
   );
 };
 
