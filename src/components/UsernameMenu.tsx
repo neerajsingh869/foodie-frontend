6555fb5313f0dc3bda77c8716b102452f3682f6c
Neeraj Singh <869neerajsingh@gmail.com>
2024-09-03 16:52:00 +0530

    refactor: remove fragment in username menu component

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -14,43 +14,41 @@ const UsernameMenu = () => {
   const { logout, user } = useAuth0();
 
   return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <img
-            src={user?.picture}
-            alt={`${user?.name} Image`}
-            className="object-contain h-12 rounded-[100%]"
-          />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-44 p-2 border-2">
-          <DropdownMenuGroup className="font-bold flex flex-col gap-1">
-            <DropdownMenuItem className="text-md  hover:bg-blue-500 focus:bg-blue-500 rounded-md hover:text-white focus:text-white">
-              <Link to="/restaurant" className="flex items-center">
-                <UtensilsCrossed className="mr-2 h-6 w-5" />
-                <span>My Restaurant</span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem className="text-md  hover:bg-blue-500 focus:bg-blue-500 rounded-md hover:text-white focus:text-white">
-              <Link to="/user-profile" className="flex items-center">
-                <User className="mr-2 h-6 w-5" />
-                <span>Profile</span>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => logout()}
-              className="text-md flex items-center text-md  hover:bg-blue-500 focus:bg-blue-500 rounded-md hover:text-white focus:text-white"
-            >
-              <LogOut className="mr-2 h-6 w-5" />
-              <span>Log out</span>
-              {/* <Button className="bg-black text-white text-lg hover:bg-black" >
-                Log Out
-              </Button> */}
-            </DropdownMenuItem>
-          </DropdownMenuGroup>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <img
+          src={user?.picture}
+          alt={`${user?.name} Image`}
+          className="object-contain h-12 rounded-[100%]"
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-44 p-2 border-2">
+        <DropdownMenuGroup className="font-bold flex flex-col gap-1">
+          <DropdownMenuItem className="text-md  hover:bg-blue-500 focus:bg-blue-500 rounded-md hover:text-white focus:text-white">
+            <Link to="/restaurant" className="flex items-center">
+              <UtensilsCrossed className="mr-2 h-6 w-5" />
+              <span>My Restaurant</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="text-md  hover:bg-blue-500 focus:bg-blue-500 rounded-md hover:text-white focus:text-white">
+            <Link to="/user-profile" className="flex items-center">
+              <User className="mr-2 h-6 w-5" />
+              <span>Profile</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => logout()}
+            className="text-md flex items-center text-md  hover:bg-blue-500 focus:bg-blue-500 rounded-md hover:text-white focus:text-white"
+          >
+            <LogOut className="mr-2 h-6 w-5" />
+            <span>Log out</span>
+            {/* <Button className="bg-black text-white text-lg hover:bg-black" >
+              Log Out
+            </Button> */}
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
