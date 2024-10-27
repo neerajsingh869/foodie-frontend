be8f3271f9b95e959f166a9f98302042a4eb463c
Neeraj Singh <869neerajsingh@gmail.com>
2024-10-27 14:21:59 +0530

    feat: create custom hook to make api request to get restaurant info of user

--- src/api/MyRestaurantApi.ts
+++ src/api/MyRestaurantApi.ts
@@ -1,9 +1,41 @@
 import { Restaurant } from "@/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  // function to make request to backend to get user details
+  const getMyRestaurantRequest = async (): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant.");
+    }
+
+    return response.json();
+  };
+
+  const {
+    data: restaurant,
+    isLoading,
+  } = useQuery({
+    queryKey: ["current-restaurant"],
+    queryFn: getMyRestaurantRequest,
+  });
+
+  return { restaurant, isLoading };
+};
 
 export const useCreateMyRestaurant = () => {
   const { getAccessTokenSilently } = useAuth0();
