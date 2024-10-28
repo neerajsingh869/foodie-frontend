ee1167a2900e013e3a45a6c5215660be2cb1470e
Neeraj Singh <869neerajsingh@gmail.com>
2024-10-28 22:57:04 +0530

    feat: change onSave method depending upon whether user is updating restaurant form or filling it for first time

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,21 +1,33 @@
 import {
   useCreateMyRestaurant,
   useGetMyRestaurant,
+  useUpdateMyRestaurant,
 } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 const ManageRestaurantPage = () => {
   const { restaurant, isLoading } = useGetMyRestaurant();
-  const { createRestaurant, isPending } = useCreateMyRestaurant();
+  const { createRestaurant, isPending: isCreatePending } =
+    useCreateMyRestaurant();
+  const { updateRestaurant, isPending: isUpdatePending } =
+    useUpdateMyRestaurant();
 
   if (isLoading) {
     return <span>ManageRestaurantPage Loading...</span>;
   }
 
+  let onSave = createRestaurant;
+  let isPending = isCreatePending;
+
+  if (restaurant) {
+    onSave = updateRestaurant;
+    isPending = isUpdatePending;
+  }
+
   return (
     <ManageRestaurantForm
       restaurant={restaurant}
-      onSave={createRestaurant}
+      onSave={onSave}
       isLoading={isPending}
     />
   );
