f38cbdc7dcb740c4d5022d9e059bd7a92c729cac
Neeraj Singh <869neerajsingh@gmail.com>
2024-11-09 14:21:58 +0530

    feat: create template for search result info part of search page

---
+++ src/components/SearchResultInfo.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  total: number;
+  city: string;
+};
+
+const SearchResultInfo = ({ total, city }: Props) => {
+  return (
+    <div className="flex flex-col lg:flex-row lg:items-center text-lg font-bold gap-3 justify-between">
+      <span>
+        {total} Restaurants found in {city}
+        <Link
+          to="/"
+          className="ml-1 text-base font-semibold underline cursor-pointer text-blue-500"
+        >
+          Choose Location
+        </Link>
+      </span>
+      <p className="text-lg">Insert sort dropdown here</p>
+    </div>
+  );
+};
+
+export default SearchResultInfo;
