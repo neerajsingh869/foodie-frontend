63531208a93c7b903814e6dd722846aa95f03ee5
Neeraj Singh <869neerajsingh@gmail.com>
2024-11-10 16:57:07 +0530

    feat: create custom hook to make backend request to get restaurant by restaurant id using react query

--- src/api/RestaurantApi.ts
+++ src/api/RestaurantApi.ts
@@ -1,8 +1,34 @@
 import { SearchState } from "@/pages/SearchPage";
-import { RestaurantSearchResponse } from "@/types";
+import { Restaurant, RestaurantSearchResponse } from "@/types";
 import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetRestaurant = (restaurantId?: string) => {
+  const getRestaurantByIdRequest = async (): Promise<Restaurant> => {
+    const response = await fetch(
+      `${API_BASE_URL}/api/restaurant/${restaurantId}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to get restaurant.");
+    }
+
+    return response.json();
+  };
+
+  const { data: restaurant, isLoading, error } = useQuery({
+    queryKey: ["fetch-restaurant"],
+    queryFn: getRestaurantByIdRequest,
+  });
+
+  if (error) {
+    toast.error(error.message);
+  }
+
+  return { restaurant, isLoading };
+};
 
 export const useSearchRestaurants = (
   searchState: SearchState,
@@ -26,11 +52,15 @@ export const useSearchRestaurants = (
     return response.json();
   };
 
-  const { data: results, isLoading } = useQuery({
+  const { data: results, isLoading, error } = useQuery({
     queryKey: ["search-restaurants", searchState],
     queryFn: createSearchRequest,
     enabled: !!city,
   });
+
+  if (error) {
+    toast.error(error.message);
+  }
 
   return { results, isLoading };
 };
