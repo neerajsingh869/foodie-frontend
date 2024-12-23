cc0d446491168160bc981d4795e9774aa2691b48
Neeraj Singh <869neerajsingh@gmail.com>
2024-12-23 23:32:27 +0530

    feat: Update Hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,9 @@
-import heroImg from "../assets/hero.png";
+import heroImg from "../assets/hero.jpg";
 
 const Hero = () => {
   return (
     <div
-      className="w-full flex justify-center items-center py-20"
+      className="w-full flex justify-center items-center sm:py-[120px] py-20"
       style={{
         background:
           "linear-gradient(166deg, rgba(59,130,246,1) 50%, rgba(245, 245, 245, 1) 50%)",
@@ -12,7 +12,7 @@ const Hero = () => {
       <img
         src={heroImg}
         alt="Hero Image"
-        className="max-h-[600px] w-[200px] sm:w-[300px] md:w-[350px] lg:w-[400px] object-contain"
+        className="max-h-[600px] w-[200px] sm:w-[300px] md:w-[350px] lg:w-[400px] object-contain rounded-[48px]"
       />
     </div>
   );
