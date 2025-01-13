1188db93e407b51fc32779761f38de11cf6a7072
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-13 21:32:03 +0530

    style: Make hero background height independent of its content

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,11 @@ import heroImg from "../assets/hero.jpg";
 
 const Hero = () => {
   return (
-    <div className="w-full flex justify-center items-center sm:py-[120px] py-20 bg-gradient-to-br from-blue-500 to-gray-100 dark:from-blue-700 dark:to-gray-900">
+    <div className="w-full flex justify-center items-center h-80 sm:h-[420px] md:h-[470px] lg:h-[506px] bg-gradient-to-br from-blue-500 to-gray-100 dark:from-blue-700 dark:to-gray-900">
       <img
         src={heroImg}
         alt="Hero Image"
-        className="max-h-[600px] w-[200px] sm:w-[300px] md:w-[350px] lg:w-[400px] object-contain rounded-[48px]"
+        className="max-h-[80%] w-[260px] sm:w-[300px] md:w-[350px] lg:w-[400px] object-contain rounded-[48px]"
       />
     </div>
   );
