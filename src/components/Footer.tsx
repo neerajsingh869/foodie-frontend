cb8f9a6343eff6ca5b34bdf48e2091948c293b4e
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-13 19:07:12 +0530

    style: Make footer part dark mode compatible

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,24 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="border-b-2 bg-blue-500 py-6">
-      <div className="container mx-auto flex flex-col gap-4 md:gap-0 md:flex-row justify-between items-center">
-        <Link to="/" className="font-bold text-3xl text-white">
-          Foodie.com
+    <footer className="border-b-2 bg-blue-500 dark:bg-blue-700 py-6">
+      <div className="container mx-auto flex flex-col gap-4 md:gap-0 md:flex-row justify-between items-center text-white">
+        <Link to="/" className="font-bold text-3xl">
+          <span className="hover:dark:text-white hover:text-gray-900 dark:text-gray-900">
+            Foodie.com
+          </span>
         </Link>
-        <div className="text-white font-bold flex gap-4">
-          <span>Privacy Policy</span>
-          <span>Terms of Service</span>
+        <div className="dark:text-gray-900 font-bold flex gap-4">
+          <span className="hover:dark:text-white hover:text-gray-900 cursor-pointer">
+            Privacy Policy
+          </span>
+          <span className="hover:dark:text-white hover:text-gray-900 cursor-pointer">
+            Terms of Service
+          </span>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
