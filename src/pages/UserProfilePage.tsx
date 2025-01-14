2a5486c23b790c39f851acbe76ca19c5d010129b
Neeraj Singh <869neerajsingh@gmail.com>
2025-01-14 13:26:44 +0530

    style: Create skeleton for user profile form

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,4 +1,5 @@
 import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
+import UserProfileFormSkeleton from "@/components/skeletons/UserProfileFormSkeleton";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
@@ -6,7 +7,7 @@ const UserProfilePage = () => {
   const { currentUser, isLoading: isGetLoading } = useGetMyUser();
 
   if (isGetLoading) {
-    return <span>UserProfilePage Loading...</span>;
+    return <UserProfileFormSkeleton />;
   }
 
   if (!currentUser) {
